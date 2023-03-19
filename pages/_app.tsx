b9293e1d6c40919268bcc7ceda111250e08e1a98
oukalatte <oukalatte@gmail.com>
2023-03-19 22:30:12 +0800

    next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import { Contents } from "@/gl/parts/contents";
 import "@/styles/style.scss";
 import type { AppProps } from "next/app";
 import { useLayoutEffect } from "react";
+import { Noto_Sans_JP } from "next/font/google";
+const font = Noto_Sans_JP({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--noto-font",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   useLayoutEffect(() => {
     new Contents(document.querySelector(".l-canvas"));
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <main className={font.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
