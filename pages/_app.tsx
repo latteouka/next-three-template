b1915d44adbf3cd3d106036530806290c9279b18
oukalatte <oukalatte@gmail.com>
2023-04-16 19:56:32 +0800

    fix mousepointer dist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/style.scss";
 import type { AppProps } from "next/app";
 import { useEffect, useRef } from "react";
 import { Noto_Sans_JP } from "next/font/google";
+import Head from "next/head";
 const font = Noto_Sans_JP({
   weight: ["400", "700"],
   subsets: ["latin"],
@@ -12,10 +13,14 @@ const font = Noto_Sans_JP({
 export default function App({ Component, pageProps }: AppProps) {
   let three = useRef<Contents>();
   useEffect(() => {
+    if (three.current) return;
     three.current = new Contents(document.querySelector(".l-canvas"));
   }, []);
   return (
     <main className={font.variable}>
+      <Head>
+        <title>Next Template</title>
+      </Head>
       <canvas className="l-canvas"></canvas>
       <Component {...pageProps} />
     </main>
